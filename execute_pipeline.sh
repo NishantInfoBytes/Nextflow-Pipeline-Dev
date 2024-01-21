766df5de10605593c6a88ab179a42e7079310a90
Nishant Shekhar <115226733+NishantInfoBytes@users.noreply.github.com>
2024-01-21 19:24:49 +0530

    Update execute_pipeline.sh

--- execute_pipeline.sh
+++ execute_pipeline.sh
@@ -1,4 +1,4 @@
-##Exporting the path of fastqc, inside this env fastqc tool is installed
+##Exporting the env path of fastqc,multiqc and nextflow, inside which yours tool are installed
 export PATH="/home/nishant.shekhar/miniconda3/envs/rnaseq-demo/bin:$PATH"
 export PATH="/home/nishant.shekhar/miniconda3/envs/nf/bin:$PATH"
 #Command to run the nextflow
